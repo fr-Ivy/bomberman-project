df1042c10d011625d1e00743b9adbb72b7d3475d
Ivy <dejongivy@gmail.com>
2025-09-03 14:27:43 +0200

    everything added

---
+++ assets/tiled/BombermanTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="BombermanTiles" tilewidth="16" tileheight="16" tilecount="322" columns="14">
+ <image source="bomberman_spritesheet.png" width="224" height="383"/>
+</tileset>
